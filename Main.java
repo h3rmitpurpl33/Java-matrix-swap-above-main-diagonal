8bf335642ddc565db01205788e9df35d07ee1958
tsomtsom <76179581+tsomtsom@users.noreply.github.com>
2021-04-20 15:03:41 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Matrix matrixA = new Matrix("Matrix A", 4, 4);
+        Matrix matrixB = new Matrix("Matrix B", 4, 4);
+
+        matrixA.RandomElements(-10, 10);
+        matrixB.RandomElements(-10, 10);
+
+        System.out.println(matrixA.toString());
+        System.out.println(matrixB.toString());
+
+        matrixA.swapElements(matrixB);
+        matrixA.printArray(matrixA);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Matrix.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.util.Random;
+
+public class Matrix {
+    private String name;
+    private int[][] elements;
+
+
+    public Matrix(String name, int numberofRows, int numberofCols) {
+        this.name = name;
+        elements = new int[numberofRows][numberofCols];
+    }
+
+    void RandomElements(int a, int b) {
+        Random number = new Random();
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = 0; j < elements[i].length; j++) {
+                elements[i][j] = number.nextInt(b + 1) + a;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String text = "";
+        text += name + "\n";
+
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = 0; j < elements[i].length; j++) {
+                text += String.format("%6d", elements[i][j]);
+            }
+            text += "\n";
+        }
+        return text;
+    }
+
+
+    public void swapElements(Matrix other) {
+        if (this.elements.length == other.elements.length &&
+                this.elements[0].length == other.elements[0].length) {
+            for (int i = 0; i < elements.length; ++i) {
+                for (int j = 0; j < elements[i].length; j++) {
+                    if (i < j) {
+                        int temp = elements[i][j];
+                        this.elements[i][j] = other.elements[i][j];
+                        other.elements[i][j] = temp;
+                    }
+                }
+            }
+        } else {
+            System.out.println("not valid");
+        }
+    }
+
+    public void printArray(Matrix matrix) {
+        for (int i = 0; i < elements.length; ++i) {
+            for (int j = 0; j < elements[0].length; ++j) {
+                System.out.println(matrix);
+            }
+            
+        }
+    }
+}
+
+
+
+
+
